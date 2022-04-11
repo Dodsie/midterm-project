055f9b3cb3df2e376b6884d30bbffdf5bd43bfce
Dodsie <baileydods@gmail.com>
2022-04-11 13:50:09 -0600

    added cart, added jquery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,25 @@
 
-console.log("hello");
+$(document).ready(function() {
+
+  $('.d-flex').on('submit', function(event) {
+    event.preventDefault();
+    window.location.href="cart";
+
+  });
+
+  $(".continue-shopping").on("submit", function(event) {
+    event.preventDefault();
+    console.log("button works again!");
+    window.location.href="menu";
+  });
+
+
+
+
+
+
+
+
+
+
+});
