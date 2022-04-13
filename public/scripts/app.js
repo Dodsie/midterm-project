0d4dbc21e77358595885587afc2427a4c21339f7
Dodsie <baileydods@gmail.com>
2022-04-13 17:38:35 -0600

    does not run, without twilio configed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,30 +3,29 @@
 $(() => {
   // Nav bar button redirects to /
   $('.navbar-brand').on('click', function() {
-    window.location.href = "/";
+    window.location.href="/";
   });
 
-  // Nav bar Cart button redirects to /orders
+// Nav bar Cart button redirects to /orders
   $('#cart-button').on('click', function(event) {
     event.preventDefault();
-    window.location.href = "/orders";
+    window.location.href="/orders";
   });
 
   // Nav bar Home button redirts to /
   $('.home-button').on('click', function(event) {
     event.preventDefault();
-    window.location.href = "/";
+    window.location.href="/";
   });
 
   $('.about-button').on('click', function(event) {
     event.preventDefault();
-    window.location.href = "/about";
+    window.location.href="/about";
   });
-//need to fix this
+//need to work on this
   $('.orders-button').on('click', function(event) {
     event.preventDefault();
-    //   window.location.href="";
-    $.get(`/users/${}`)
+    window.location.href="users/2/myOrders";
   });
 
 
--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const sendSMS = require('../sendSMS')
-
+const Order = require('../models/order');
 //localhost:8080/orders/
 module.exports = (db) => {
   router.get("/menu", (req, res) => {
@@ -59,7 +58,90 @@ module.exports = (db) => {
 
 
 
-  return router;
+  //ROUTER TWILLIO -------->>>>
+  // GET: /orders
+  // router.get('/', function(_req, res) {
+  //   Order.find().then(function(orders) {
+  //     res.render('orders/index', {orders});
+  //   });
+  // });
+
+  // // GET: /orders/4
+  // router.get('/:id/show', function(req, res) {
+  //   const id = req.params.id;
+  //   Order.findOne({_id: id}).then(function(order) {
+  //     res.render('orders/show', {order: order});
+  //   });
+  // });
+
+  // // POST: /orders/4/pickup
+  // router.post('/:orderId/pickup', function(req, res) {
+  //   const id = req.params.orderId;
+
+  //   Order.findOne({_id: id}).then(function(order) {
+  //     order.status = 'Shipped';
+  //     order.notificationStatus = 'Queued';
+
+  //     order.save()
+  //       .then(function() {
+  //         return order.sendSmsNotification('Your clothes will be sent and will be delivered in 20 minutes', getCallbackUri(req));
+  //       })
+  //       .then(function() {
+  //         res.redirect(`/orders/${id}/show`);
+  //       })
+  //       .catch(function(err) {
+  //         res.status(500).send(err.message);
+  //       });
+  //   });
+  // });
+
+  // // POST: /orders/4/deliver
+  // router.post('/:orderId/deliver', function(req, res) {
+  //   const id = req.params.orderId;
+
+  //   Order.findOne({_id: id})
+  //     .then(function(order) {
+  //       order.status = 'Delivered';
+  //       order.notificationStatus = 'Queued';
+  //       order.save()
+  //         .then(function() {
+  //           return order.sendSmsNotification('Your clothes have been delivered', getCallbackUri(req));
+  //         })
+  //         .then(function() {
+  //           res.redirect(`/orders/${id}/show`);
+  //         })
+  //         .catch(function(err) {
+  //           res.status(500).send(err.message);
+  //         });
+  //     });
+  // });
+
+
+  // // POST: /orders/4/status/update
+  // router.post('/:orderId/status/update', function(req, res) {
+  //   const id = req.params.orderId;
+
+  //   const notificationStatus = req.body.MessageStatus;
+
+  //   Order.findOne({_id: id})
+  //     .then(function(order) {
+  //       order.notificationStatus = notificationStatus.charAt(0).toUpperCase() + notificationStatus.slice(1);
+  //       return order.save();
+  //     })
+  //     .then(function() {
+  //       res.sendStatus(200);
+  //     })
+  //     .catch(function(err) {
+  //       res.status(500).send(err.message);
+  //     });
+  // });
+
+  // function getCallbackUri(req) {
+  //   return `http://${req.headers.host}/orders/${req.params.orderId}/status/update`;
+  // }
+
+
+  // return router;
 };
 
 
