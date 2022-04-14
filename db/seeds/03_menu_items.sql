258703a7918924ae84d112a243e20e8e3e317401
Dodsie <baileydods@gmail.com>
2022-04-14 16:00:22 -0600

    done?

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,11 +1,10 @@
 INSERT INTO menu_items (name, price, photo, description)
-VALUES ('Nally', 10, 'https://images.pexels.com/photos/2825225/pexels-photo-2825225.jpeg?auto=compress&cs=tinysrgb&w=245', 'I am a teacher 1');
-
+VALUES ('Nally', 92, 'https://i.pinimg.com/280x280_RS/55/f3/3b/55f33b92b03b85d02c0a65f4aec4699a.jpg', 'Hold my beer and watch this.');
 INSERT INTO menu_items (name, price, photo, description )
-VALUES ('Andy', 11, 'https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg?auto=compress&cs=tinysrgb&dpr=b&w=240', 'I am a teacher 2' );
+VALUES ('Andy', 97, 'https://imgix.ranker.com/user_node_img/82/1625047/original/mr-incredible-film-characters-photo-u2?w=140&h=140&q=90&fm=jpg&fit=crop&crop=faces', 'Professional ebook voice actor.' );
 INSERT INTO menu_items (name, price, photo, description)
-VALUES ('David', 16, 'https://images.pexels.com/photos/725997/pexels-photo-725997.jpeg?auto=compress&cs=tinysrgb&w=240', 'I am a teacher 3');
+VALUES ('David', 69, 'https://img.moviequotes.com/images/thumb_person-jason-momoa.140x140_q95_box-149,0,695,546.jpg', '/dinosaur/cars/WoW');
 INSERT INTO menu_items (name, price, photo, description)
-VALUES ('TeacherA', 2, 'https://images.pexels.com/photos/4552047/pexels-photo-4552047.jpeg?auto=compress&cs=tinysrgb&w=240', 'I am a teacher 4');
+VALUES ('Edward', 99, 'https://m.media-amazon.com/images/M/MV5BMDZhZmFjZDMtNjIyMC00N2I4LWI1ZmQtN2UyYmFmMGU3NmFhXkEyXkFqcGdeQXVyMzkyOTg1MzE@._V1_QL75_UY140_CR56,0,140,140_.jpg', 'Anonymous.');
 INSERT INTO menu_items (name, price, photo, description)
-VALUES ('TeacherB', 20, 'https://image.shutterstock.com/image-photo/addis-ababa-ethiopia-january-30-400w-1312694819.jpg', 'I am a teacher 5');
+VALUES ('T', 20, 'https://image.shutterstock.com/image-photo/addis-ababa-ethiopia-january-30-400w-1312694819.jpg', '');
